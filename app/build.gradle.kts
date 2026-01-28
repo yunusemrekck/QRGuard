5e7c376585a1131dc1e6541cc890cd149ad6d62e
YunusEmre <yunusemekck@gmail.com>
2026-01-28 10:29:58 +0300

    Ui düzenlemeleri.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
     // Core
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
